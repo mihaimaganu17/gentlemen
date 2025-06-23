bbd005f34313c7f2109c2a3f8dd955e768ec9af0
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-06-23 11:07:53 +0200

    A bit of information flow-control theory

--- src/lib.rs
+++ src/lib.rs
@@ -100,6 +100,38 @@ pub struct Task {
     _datastores: Vec<Datastore>,
 }
 
+enum _Confidentiality {
+    High(_Secret),
+    Low(_Public),
+}
+
+// Public <= Secret (partial order)
+struct _Public;
+struct _Secret;
+
+enum _Integrity {
+    Trusted(_HighIntegrity),
+    Untrusted(_LowIntegrity),
+}
+
+struct _HighIntegrity;
+struct _LowIntegrity;
+
+// Planners get instrumented with dynamic information-flow control via taint-tracking. For this,
+// labels are attached to messages, actions, tool arguments and results, and vairables in the
+// datastore.
+//
+// Labels originate from data read by tools from the datastore, which tools propagate to their
+// results,
+// the planner propagates from messages to actions
+// and the planning loop propagates throughout its execution.
+//
+// We add a metadata field to label each node in the syntax tree of tools results.
+// When non-empty, such a label applied to all field of that node and below.
+//
+// Also attach metadata field to label individual messages in the conversation history.
+// The initial system and user messages are typically considered trusted and public and by default.
+
 // A trace is a sequence of actions that the model takes starting from a user's Message::Query
 // and ending with an `Action::Finish`.
 pub struct Trace(pub Vec<Action>);
