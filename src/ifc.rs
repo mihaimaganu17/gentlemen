6b0d359eb45dd639f96879b842ededc24ad5ef9a
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-06-28 11:00:28 +0200

    Added product ordering

--- src/ifc.rs
+++ src/ifc.rs
@@ -91,3 +91,51 @@ impl Integrity {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Product<A: Lattice, B: Lattice> {
+    lattice1: A,
+    lattice2: B,
+}
+
+impl<A: Lattice, B: Lattice> PartialOrd for Product<A, B> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let ord1 = self.lattice1.partial_cmp(&other.lattice1)?;
+        let ord2 = self.lattice2.partial_cmp(&other.lattice2)?;
+        if ord1 == ord2 {
+            // If the 2 are equal, we return the result
+            Some(ord1)
+        } else {
+            if ord1 == Ordering::Less && ord2 == Ordering::Equal
+                || ord1 == Ordering::Equal && ord2 == Ordering::Less {
+                Some(Ordering::Less)
+            } else {
+                Some(Ordering::Greater)
+            }
+        }
+    }
+}
+
+impl<A: Lattice, B: Lattice> Lattice for Product<A, B> {
+    /// Returns the least upper bound between `self` and `other` values
+    fn join(self, other: Self) -> Self {
+        let lattice1 = self.lattice1.join(other.lattice1);
+        let lattice2 = self.lattice2.join(other.lattice2);
+
+        Self {
+            lattice1,
+            lattice2,
+        }
+    }
+
+    /// Returns the greatest lower bound between `self` and `other` values
+    fn meet(self, other: Self) -> Self {
+        let lattice1 = self.lattice1.meet(other.lattice1);
+        let lattice2 = self.lattice2.meet(other.lattice2);
+
+        Self {
+            lattice1,
+            lattice2,
+        }
+    }
+}
+
