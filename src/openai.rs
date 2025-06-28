24c36895021071591ac0b24d2a9a78339d41a12d
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-06-28 13:35:20 +0200

    Basic OpenAI compatible client API

--- src/openai.rs
+++ src/openai.rs
@@ -1,4 +1,37 @@
-use async_openai::{Client, config::OpenAIConfig, types::CreateCompletionRequestArgs};
+use async_openai::{Client, config::OpenAIConfig, types::{CreateCompletionRequestArgs, CreateCompletionResponse}, error::OpenAIError};
+
+pub struct LlmClient {
+    client: Client<OpenAIConfig>,
+}
+
+impl LlmClient {
+    pub fn new(api_key: &str, api_base: &str) -> Self {
+        let config = OpenAIConfig::new()
+            .with_api_key(api_key)
+            .with_api_base(api_base)
+            .with_org_id("buciumede");
+
+        let client = Client::with_config(config);
+        Self {
+            client
+        }
+    }
+
+    pub async fn completion(&self, model: &str, prompt: &str) -> Result<CreateCompletionResponse, OpenAIError> {
+        // Create a `CreateCompletionRequest`
+        let request = CreateCompletionRequestArgs::default()
+            .model(model)
+            .prompt(prompt)
+            .max_tokens(100_u32)
+            .build()?;
+
+        let response = self.client
+            .completions()
+            .create(request)
+            .await?;
+        Ok(response)
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -8,32 +41,14 @@ mod tests {
     async fn openai_local_llama32_demo() {
         let api_key = ""; //env!("OPENAI_API_KEY");
         let api_base = "http://localhost:11434/v1";
-        let config = OpenAIConfig::new()
-            .with_api_key(api_key)
-            .with_api_base(api_base)
-            .with_org_id("buciumede");
 
-        let client = Client::with_config(config);
+        let client = LlmClient::new(api_key, api_base);
 
-        {
-            // let model = "gpt-3.5-turbo-instruct";
-            let model = "llama3.2";
-            let prompt = "Tell me the recipe of alfredo pasta";
+        // let model = "gpt-3.5-turbo-instruct";
+        let model = "llama3.2";
+        let prompt = "Tell me the recipe of alfredo pasta";
+        let response = client.completion(model, prompt).await.expect("Failed to request model completion");
 
-            // Create a `CreateCompletionRequest`
-            let request = CreateCompletionRequestArgs::default()
-                .model(model)
-                .prompt(prompt)
-                .max_tokens(100_u32)
-                .build()
-                .unwrap();
-
-            let response = client
-                .completions()
-                .create(request)
-                .await
-                .unwrap();
-            println!("{}", response.choices.first().unwrap().text);
-        }
+        println!("{}", response.choices.first().unwrap().text);
     }
 }
