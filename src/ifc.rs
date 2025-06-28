011aeed359e34ec0bdc5401f360ba0deb0175c30
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2025-06-28 12:08:17 +0200

    Added PowersetLattice implementation

--- src/ifc.rs
+++ src/ifc.rs
@@ -1,10 +1,14 @@
-use std::cmp::Ordering;
+use std::{
+    collections::HashSet,
+    hash::Hash,
+    cmp::Ordering
+};
 
-pub trait Lattice: PartialOrd {
+pub trait Lattice: PartialOrd + Sized {
     /// Returns the least upper bound between `self` and `other` values
-    fn join(self, other: Self) -> Self;
+    fn join(self, other: Self) -> Option<Self>;
     /// Returns the greatest lower bound between `self` and `other` values
-    fn meet(self, other: Self) -> Self;
+    fn meet(self, other: Self) -> Option<Self>;
 }
 
 #[derive(Debug, PartialEq)]
@@ -22,20 +26,20 @@ impl PartialOrd for Confidentiality {
 }
 
 impl Lattice for Confidentiality {
-    fn join(self, other: Self) -> Self {
-        if self <= other {
+    fn join(self, other: Self) -> Option<Self> {
+        Some(if self <= other {
             other
         } else {
             self
-        }
+        })
     }
 
-    fn meet(self, other: Self) -> Self {
-        if self <= other {
+    fn meet(self, other: Self) -> Option<Self> {
+        Some(if self <= other {
             self
         } else {
             other
-        }
+        })
     }
 }
 
@@ -64,20 +68,20 @@ impl PartialOrd for Integrity {
 }
 
 impl Lattice for Integrity {
-    fn join(self, other: Self) -> Self {
-        if self <= other {
+    fn join(self, other: Self) -> Option<Self> {
+        Some(if self <= other {
             other
         } else {
             self
-        }
+        })
     }
 
-    fn meet(self, other: Self) -> Self {
-        if self <= other {
+    fn meet(self, other: Self) -> Option<Self> {
+        Some(if self <= other {
             self
         } else {
             other
-        }
+        })
     }
 }
 
@@ -117,21 +121,30 @@ impl<A: Lattice, B: Lattice> PartialOrd for Product<A, B> {
 
 impl<A: Lattice, B: Lattice> Lattice for Product<A, B> {
     /// Returns the least upper bound between `self` and `other` values
-    fn join(self, other: Self) -> Self {
-        let lattice1 = self.lattice1.join(other.lattice1);
-        let lattice2 = self.lattice2.join(other.lattice2);
+    fn join(self, other: Self) -> Option<Self> {
+        let lattice1 = self.lattice1.join(other.lattice1)?;
+        let lattice2 = self.lattice2.join(other.lattice2)?;
 
-        Self {
+        Some(Self {
             lattice1,
             lattice2,
-        }
+        })
     }
 
     /// Returns the greatest lower bound between `self` and `other` values
-    fn meet(self, other: Self) -> Self {
-        let lattice1 = self.lattice1.meet(other.lattice1);
-        let lattice2 = self.lattice2.meet(other.lattice2);
+    fn meet(self, other: Self) -> Option<Self> {
+        let lattice1 = self.lattice1.meet(other.lattice1)?;
+        let lattice2 = self.lattice2.meet(other.lattice2)?;
 
+        Some(Self {
+            lattice1,
+            lattice2,
+        })
+    }
+}
+
+impl<A: Lattice, B: Lattice> Product<A, B> {
+    pub fn new(lattice1: A, lattice2: B) -> Self {
         Self {
             lattice1,
             lattice2,
@@ -139,3 +152,56 @@ impl<A: Lattice, B: Lattice> Lattice for Product<A, B> {
     }
 }
 
+#[derive(Debug, PartialEq)]
+pub struct PowersetLattice<T: Eq + Hash> {
+    subset: HashSet<T>,
+    universe: HashSet<T>,
+}
+
+impl<T: Eq + Hash> PowersetLattice<T> {
+    pub fn new(subset: HashSet<T>, universe: HashSet<T>) -> Result<Self, LatticeError> {
+        if !subset.is_subset(&universe) {
+            return Err(LatticeError::SubsetNotInUniverse);
+        }
+
+        Ok(Self {
+            subset,
+            universe
+        })
+    }
+}
+
+impl<T: Eq + Hash> PartialOrd for PowersetLattice<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        if self.subset == other.subset {
+            Some(Ordering::Equal)
+        } else if self.subset.is_subset(&other.subset) {
+            Some(Ordering::Less)
+        } else {
+            Some(Ordering::Greater)
+        }
+    }
+}
+
+impl<T: Eq + Hash + Clone> Lattice for PowersetLattice<T> {
+    /// Returns the least upper bound between `self` and `other` values
+    fn join(self, other: Self) -> Option<Self> {
+        // Union of the 2 subsets
+        let subset = &self.subset | &other.subset;
+
+        Self::new(subset, self.universe).ok()
+    }
+
+    /// Returns the greatest lower bound between `self` and `other` values
+    fn meet(self, other: Self) -> Option<Self> {
+        // Intersection of the 2 subsets
+        let subset = &self.subset & &other.subset;
+
+        Self::new(subset, self.universe).ok()
+    }
+}
+
+#[derive(Debug)]
+pub enum LatticeError {
+    SubsetNotInUniverse,
+}
